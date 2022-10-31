cd7822b0165088f926d20cd058233ed424adfc22
Bryan Lorden <bryan.lorden22@gmail.com>
2022-10-31 00:27:55 -0500

    Added console based and WebApp all in repo

--- ConsoleBased/KickerPlayerModel.java
+++
@@ -1,8 +0,0 @@
-public class KickerPlayerModel extends PlayerModel {
-    private static final double KICKERALLOWEDREACH = 0.5;
-    public static final String POSITIONSHORTHANDLE = "K";
-
-    public KickerPlayerModel(String firstName, String lastName, double predictedScore, double avgADP) {
-        super(firstName, lastName, POSITIONSHORTHANDLE, predictedScore, avgADP, KICKERALLOWEDREACH);
-    }
-}
--- WebApp/Backend/target/classes/com/example/Mock/DraftSim/README.md
+++
@@ -1,4 +0,0 @@
-# 2022-Fantasy-Football-Mock-Draft-Simulator
-A personal project to build a fantasy football mock draft simulator
-
-Terminal version in progress. Web frontend to be added later, and eventually converted into a neural network.
